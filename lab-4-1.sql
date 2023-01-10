90b4f7b6ec2a15f3cfbf45363849eb02dbbdcf9c
Eric-Onsager <eric.onsager@kellogg.northwestern.edu>
2023-01-10 22:25:58 +0000

    SQL Practice

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,9 @@
 -- How many lifetime hits does Barry Bonds have?
 
+SELECT players.last_name, sum(stats.hits) 
+FROM stats 
+INNER JOIN players ON stats.player_id = players.id
+WHERE players.last_name = "Bonds" AND players.first_name = "Barry";
 -- Expected result:
 -- 2935
 
